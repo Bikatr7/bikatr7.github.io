83ea7f309fcd43d0ac072845629263ab46de2c74
Bikatr7 <Tetralon07@gmail.com>
2024-04-12 17:15:47 -0600

    renamed some files, fixed the footer being weird
    switch project buttons are gone now though lol

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -1,8 +1,11 @@
 // for the header and footer to be sticky
-window.addEventListener("load", function() {
+window.addEventListener("DOMContentLoaded", adjustPadding);
+window.addEventListener("resize", adjustPadding);
+
+function adjustPadding() {
   var headerHeight = document.querySelector('header').offsetHeight;
   var footerHeight = document.querySelector('footer').offsetHeight;
 
   document.body.style.paddingTop = headerHeight + 'px';
   document.body.style.paddingBottom = footerHeight + 'px';
-});
+}
